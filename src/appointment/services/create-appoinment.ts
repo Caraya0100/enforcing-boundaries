83a508f366e61c955cf9ed8f19fadca2d02b2d59
Rubio <caraya0100@gmail.com>
2024-10-03 21:29:48 -0300

    feat: update vertical slices

--- src/appointment/services/create-appoinment.ts
+++ src/appointment/services/create-appoinment.ts
@@ -1,4 +1,5 @@
 import { Appointment } from '@/appointment/entities/appointment';
+// Uncomment the next line to see the linter error
 // import { Doctor } from '@/doctor/entities/doctor';
 
 export function createAppoinment(appointment: Appointment) {
